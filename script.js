6672877a92b2d0ca28b3f4635320afb55c7d40dc
Janhavi Belnekar <114862128+Janhavi30052002@users.noreply.github.com>
2023-08-19 19:34:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,78 +1,49 @@
-let startBtn = document.getElementById('start');
-let stopBtn = document.getElementById('stop');
-let resetBtn = document.getElementById('reset');
+let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+let timeRef = document.querySelector(".timer-display");
+let int = null;
 
-let hour = 00;
-let minute = 00;
-let second = 00;
-let count = 00;
-
-startBtn.addEventListener('click', function () {
-	timer = true;
-	stopWatch();
+document.getElementById("start-timer").addEventListener("click", () => {
+    if(int !== null) {
+        clearInterval(int);
+    }
+    int = setInterval(displayTimer, 10);
 });
 
-stopBtn.addEventListener('click', function () {
-	timer = false;
+document.getElementById("pause-timer").addEventListener("click", () => {
+    clearInterval(int);
 });
 
-resetBtn.addEventListener('click', function () {
-	timer = false;
-	hour = 0;
-	minute = 0;
-	second = 0;
-	count = 0;
-	document.getElementById('hr').innerHTML = "00";
-	document.getElementById('min').innerHTML = "00";
-	document.getElementById('sec').innerHTML = "00";
-	document.getElementById('count').innerHTML = "00";
-});
+document.getElementById("reset-timer").addEventListener("click", () => {
+    clearInterval(int);
+    [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+    timeRef.innerHTML = "00 : 00 : 00 : 000 ";
+}); 
 
-function stopWatch() {
-	if (timer) {
-		count++;
+function displayTimer() {
+    milliseconds += 10;
+    if(milliseconds == 1000) {
+        milliseconds = 0;
+        seconds++;
+        if(seconds == 60) {
+            seconds = 0;
+            minutes++;
+            if(minutes == 60) {
+                minutes = 0;
+                hours++;
+            }
+        }
+    }
 
-		if (count == 100) {
-			second++;
-			count = 0;
-		}
+    let h = hours < 10 ? "0" + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
+    let ms = 
+        milliseconds < 10
+        ? "00" + milliseconds
+        : milliseconds < 100
+        ? "0" + milliseconds
+        : milliseconds;
 
-		if (second == 60) {
-			minute++;
-			second = 0;
-		}
+    timeRef.innerHTML = `${h} : ${m} : ${s} : ${ms}`;
 
-		if (minute == 60) {
-			hour++;
-			minute = 0;
-			second = 0;
-		}
-
-		let hrString = hour;
-		let minString = minute;
-		let secString = second;
-		let countString = count;
-
-		if (hour < 10) {
-			hrString = "0" + hrString;
-		}
-
-		if (minute < 10) {
-			minString = "0" + minString;
-		}
-
-		if (second < 10) {
-			secString = "0" + secString;
-		}
-
-		if (count < 10) {
-			countString = "0" + countString;
-		}
-
-		document.getElementById('hr').innerHTML = hrString;
-		document.getElementById('min').innerHTML = minString;
-		document.getElementById('sec').innerHTML = secString;
-		document.getElementById('count').innerHTML = countString;
-		setTimeout(stopWatch, 10);
-	}
 }
